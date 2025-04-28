02c2659882eeec91ee36979428f669052d6598d1
Nisarg153 <nisarg150301@gmail.com>
2025-04-28 13:46:02 -0400

    after-install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,8 +4,8 @@ set -e
 echo "Installing backend dependencies after deployment..."
 
 # Fix permissions
-sudo chown -R ec2-user:ec2-user /home/ec2-user/myapp
+sudo chown -R ec2-user:ec2-user /home/ec2-user/application-code
 
 # Now install backend dependencies
-cd /home/ec2-user/myapp/backend
+cd /home/ec2-user/application-code/app-tier
 npm install
